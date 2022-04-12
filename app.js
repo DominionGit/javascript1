99ac0ecbcfba5c6abda1f4e2ccb62419ee6cb14a
DominionGit <dominionmichael2@gmail.com>
2022-04-12 01:52:59 -0800

    javascript

---
+++ app.js
@@ -0,0 +1,20 @@
+var num1 = prompt("input your first num1");
+var num2 =  prompt("input your last num2");
+var num1 = 30
+var num2 = 35
+var user = 30 + 35
+console.log('the sum of ' + num1 + ' + num2  is ' + user);
+ alert(output);
+
+    
+
+var firstname = "dominion";
+var name = prompt("input your first name");
+var lastname = "michael"
+var last = prompt("input your last name");
+var firstnames = firstname.slice(0,4);
+var lastnames = lastname.slice(0,1);
+var names = firstnames + lastnames;
+
+var output = "your capacitybay user id is " + names;
+alert(output)
